5eb684115a187f4bab1e7ae889ecf11619118409
unknown <adhirajnanda@gmail.com>
2023-03-31 16:19:33 +0530

    MusicPlayer..

---
+++ data.js
@@ -0,0 +1,26 @@
+let songs = [
+  {
+    name: "song 1",
+    path: "./s2.mp3",
+    artist: "artist 1",
+    cover: "./2.png",
+  },
+  {
+    name: "song 2",
+    path: "./s1.mp3",
+    artist: "artist 2",
+    cover: "./3.png",
+  },
+  {
+    name: "song 3",
+    path: "./s3.mp3",
+    artist: "artist 3",
+    cover: "./4.png",
+  },
+  {
+    name: "song 4",
+    path: "./s4.mp3",
+    artist: "artist 4",
+    cover: "./4.png",
+  },
+];
